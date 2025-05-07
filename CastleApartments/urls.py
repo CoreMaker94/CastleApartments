3191fe28c339720b922749ef9193f673f1565fb3
yeabsiratesfaye <tesfyabsera18@gmail.com>
2025-05-07 09:54:03 +0000

    adde views and urls still need to work on some of them

--- CastleApartments/urls.py
+++ CastleApartments/urls.py
@@ -19,4 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('property/',include('property.urls')),
+    path('purchaseoffer/'include('purchaseoffer.urls')),
 ]
--- purchaseoffer/views.py
+++ purchaseoffer/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import HttpResponse
 from django.shortcuts import render
+from purchaseoffer.models import Offer
 
 # Create your views here.
+def offer_list(request):
+    offer = offers.objects.all()
+    return render(request, "purchaseoffer/purchaseoffers.html", {
+        'offer': offer,
+    })
+def finalized_offer_detail(request, offer_id):
+    # finsih this
