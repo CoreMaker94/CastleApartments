a61d0542f927e10ee1bfbaf5eddf8c165e2063cc
CoreMaker94 <87919895+CoreMaker94@users.noreply.github.com>
2025-05-08 13:07:24 +0000

    Added image functionality for profiles and connected it to proper html files

--- CastleApartments/settings.py
+++ CastleApartments/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
         },
     },
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 WSGI_APPLICATION = 'CastleApartments.wsgi.application'
 
@@ -126,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
