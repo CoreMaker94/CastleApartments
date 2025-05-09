31af937e5f3433322411a55f18c02db7752eac14
CoreMaker94 <87919895+CoreMaker94@users.noreply.github.com>
2025-05-09 11:24:52 +0000

    Removed references to userprofile and app starts

--- userprofile/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.profile, name='profile'),
-    path('<int:id>', views.get_profile_by_id, name='profile-by-id'),
-]
